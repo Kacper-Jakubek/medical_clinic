d84234d50f5cc4ad4e300285efbb7ab489680ce5
Jan Wadolowski <janwadolowski@gmail.com>
2024-12-10 01:51:31 +0100

    silent code cleanup

--- src/main/java/pl/wsb/lab/medicalclinic/schedule/ScheduleRepository.java
+++ src/main/java/pl/wsb/lab/medicalclinic/schedule/ScheduleRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface ScheduleRepository {
-    public Optional<Schedule> findByDoctorId(UUID doctorId);
+    Optional<Schedule> findByDoctorId(UUID doctorId);
 
-    public void addSchedule(Schedule schedule);
+    void addSchedule(Schedule schedule);
 }
